0337994dad4de4c1e71737e6bad54c817275f453
Yitshhaq Fukushima <ymotse@gmail.com>
2020-03-10 20:41:46 -0300

    Share initial project.

---
+++ src/main/java/br/com/ymotse/api/MicroserviceJavaServiceUsersApplication.java
@@ -0,0 +1,13 @@
+package br.com.ymotse.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceJavaServiceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceJavaServiceUsersApplication.class, args);
+	}
+
+}
